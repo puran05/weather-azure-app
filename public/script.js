3e9279db10ca14b69839018f66fac85e7e330ca6
puran05 <77312684+puran05@users.noreply.github.com>
2025-01-02 23:53:14 -0600

    api fetched from the weather app sucessfully

--- public/script.js
+++ public/script.js
@@ -1,4 +1,33 @@
 const weatherForm = document.getElementById("weatherForm");
+// const apiUrl = "https://jsonplaceholder.typicode.com/users";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+const apiKey = "182621373de075820827e7812a896779";
+
+async function fetchWeather(city, temperature) {
+  try {
+    const response = await fetch(
+      `${apiUrl}?q=${city}&appid=${apiKey}&units=imperial`
+    );
+
+    if (!response.ok) {
+      throw new Error(`Http error ! Status:${response.status}`);
+    }
+
+    const data = await response.json();
+    const temperatureReturned = data.main.temp;
+    console.log(temperature);
+    if (temperatureReturned < temperature) {
+      console.log("Alert! It is cold outside");
+    } else {
+      console.log("Worry not! You can handle the weather today");
+    }
+
+    console.log(`The temperature of ${city} is ${temperatureReturned}`);
+    // console.log("Fetched Data with Headers:", data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
 
 weatherForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -7,6 +36,12 @@ weatherForm.addEventListener("submit", (event) => {
   const email = document.getElementById("email").value;
   const city = document.getElementById("city").value;
   const temperature = document.getElementById("temperature").value;
+
+  if ((city, temperature)) {
+    fetchWeather(city, temperature);
+  } else {
+    console.error("city not found");
+  }
 
   console.log(fullname, city, email, temperature);
 });
